67015bf6e299f560fb0b9b952acce795a04e37e9
Aaron Tse <tse16@purdue.edu>
2025-06-11 21:25:23 -0700

    starting to research google implementation

--- imagetoexcel.py
+++ imagetoexcel.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
 import boto3
 import trp
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 textract = boto3.client('textract')
 
