2cb0fec7a48f6e26c4275c27ee5039abc8cd938c
Aaron Tse <tse16@purdue.edu>
2025-06-16 22:32:54 -0400

    figured out the google sheets API

--- imagetoexcel.py
+++ imagetoexcel.py
@@ -6,4 +6,9 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 textract = boto3.client('textract')
+s3 = boto3.client('s3')
+
+SERVICE_ACCOUNT_FILE = 'aws-textract-integration-635b96f154ed.json'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SPREADSHEET_ID = os.environ['SPREADSHEET_ID']
 
